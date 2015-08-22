69699e5f732221026a0f2842add89fb9e0281699
Patrice Vignola <vignola.patrice@gmail.com>
2015-08-22 00:45:35 -0400

    Add the character movement depending on the camera rotation

--- Assets/Scripts/ChildController.cs
+++ Assets/Scripts/ChildController.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using InputHandler;
 
-public class CharacterController : MonoBehaviour {
+[RequireComponent(typeof(Child))]
+public class ChildController : MonoBehaviour
+{
+    public enum Player { One, Two, Three, Four }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Player PlayerNumber;
+
+    private Child _child;
+
+    void Awake()
+    {
+        InputManager.Instance.PushActiveContext("Gameplay", (int)PlayerNumber);
+        InputManager.Instance.AddCallback((int)PlayerNumber, HandlePlayerInput);
+
+        _child = GetComponent<Child>();
+    }
+
+    private void HandlePlayerInput(MappedInput input)
+    {
+        if (this == null) return;
+
+        float xValue = 0f;
+
+        if (input.Ranges.ContainsKey("MoveLeft"))
+        {
+            xValue = -input.Ranges["MoveLeft"];
+        }
+        else if (input.Ranges.ContainsKey("MoveRight"))
+        {
+            xValue = input.Ranges["MoveRight"];
+        }
+
+        float zValue = 0f;
+
+        if (input.Ranges.ContainsKey("MoveForward"))
+        {
+            zValue = input.Ranges["MoveForward"];
+        }
+        else if (input.Ranges.ContainsKey("MoveBackward"))
+        {
+            zValue = -input.Ranges["MoveBackward"];
+        }
+
+        _child.Move(xValue, zValue);
+    }
+
 }
