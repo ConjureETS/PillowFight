a4aa35f554f63e629e9b91670cdfb7260946c08e
jparent <jeanp.parent@gmail.com>
2015-08-22 01:05:40 -0400

    add basic Pillow and pickup when in proximity

--- Assets/Scripts/Child.cs
+++ Assets/Scripts/Child.cs
@@ -7,12 +7,27 @@ public class Child : MonoBehaviour
     public float Speed = 10f;
 
     private Rigidbody _rb;
+    public Pillow pillow;
+
 
     void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
+
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Pillow") {
+            
+            pillow = other.GetComponent<Pillow>();
+            other.transform.parent = transform; // make the pillow a child of Child
+            
+            // TODO: place the pillow correctly or animate or something...
+
+        }
+    }
+   
     public void Move(float xValue, float zValue)
     {
         // We move the child depending on the camera orientation
