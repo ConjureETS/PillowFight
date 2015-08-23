af0053f169ee2438b142d692a6921b06cdd0a01f
Patrice Vignola <vignola.patrice@gmail.com>
2015-08-23 03:18:31 -0400

    Add visual cue for selectable pillows

--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -1,31 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(MeshRenderer))]
 public class Floor : MonoBehaviour
 {
     public MomBehavior Mom;
     public Material NormalMaterial;
     public Material LavaMaterial;
 
-    private MeshRenderer _renderer;
+    public MeshRenderer Renderer;
 
     void Awake()
     {
-        _renderer = GetComponent<MeshRenderer>();
         Mom.OnEnterRoom += ChangeToNormalFloor;
         Mom.OnLeaveRoom += ChangeToLavaFloor;
     }
 
     private void ChangeToNormalFloor()
     {
-        _renderer.material = NormalMaterial;
+        Renderer.material = NormalMaterial;
         gameObject.tag = "Floor"; // Might not be necessary since the player is most likely "dead" if he touches a non-lava floor
     }
 
     private void ChangeToLavaFloor()
     {
-        _renderer.material = LavaMaterial;
+        Renderer.material = LavaMaterial;
         gameObject.tag = "Lava"; // Might not be necessary since the player is most likely "dead" if he touches a non-lava floor
     }
 
--- Assets/Scripts/Pillow.cs
+++ Assets/Scripts/Pillow.cs
@@ -2,26 +2,41 @@
 using System.Collections;
 
 public class Pillow : MonoBehaviour {
-    
+
+    public Color SelectableMinColor;
+    public float LerpDuration = 2f;
+
     public bool IsThrown = false;
     
     private bool IsPickable = true;
 
     private Collider _col;
     private Rigidbody _rb;
+    private MeshRenderer _renderer;
 
     private bool _isOwned;
+    private Color _defaultColor;
+
+    private float _ratio = 0f;
+    private bool _lerpingUp = false;
 
     public bool IsOwned
     {
         get { return _isOwned; }
-        set { _isOwned = value; }
+        set
+        {
+            _isOwned = value;
+            _renderer.material.color = _defaultColor;
+        }
     }
 
 	// Use this for initialization
 	void Start () {
         _col = GetComponent<Collider>();
         _rb = GetComponent<Rigidbody>();
+        _renderer = GetComponent<MeshRenderer>();
+
+        _defaultColor = _renderer.material.color;
 	}
 	
 	// Update is called once per frame
@@ -29,6 +44,27 @@ public class Pillow : MonoBehaviour {
 
         if (transform.position.y < -1) {
             Destroy(this.gameObject);
+        }
+
+        if (!_isOwned)
+        {
+            _ratio += Time.deltaTime / LerpDuration / 2f;
+
+            if (_lerpingUp)
+            {
+                _renderer.material.color = Color.Lerp(SelectableMinColor, _defaultColor, _ratio);
+            }
+            else
+            {
+                _renderer.material.color = Color.Lerp(_defaultColor, SelectableMinColor, _ratio);
+            }
+
+            if (_ratio >= 1f)
+            {
+                _lerpingUp = !_lerpingUp;
+
+                _ratio = 0f;
+            }
         }
         
 	}
@@ -48,6 +84,7 @@ public class Pillow : MonoBehaviour {
         _rb.isKinematic = false;
         _col.enabled = true;
         _rb.AddForce(force, ForceMode.Impulse);
+        _renderer.material.color = _defaultColor;
     }
 
     public void MakePickable() {
